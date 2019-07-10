72b06f32b4141fc51061ec00438ef5cca1505203
Rob Ballantyne <raballantyne@gmail.com>
2019-07-10 18:20:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,6 @@ def read_from_hash(hash, key)
   hash[key]
 
 def update_counting_hash(hash, key)
-  hash[key] ? hash[key] : hash[key] += 1 
+  hash[key] ? hash[key] += 1 : hash[key] = 1
   hash
 end
